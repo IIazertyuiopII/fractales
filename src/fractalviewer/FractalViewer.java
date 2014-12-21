9c71c37a17450118c566d443abacba754d28a1ee
iiazertyuiopii <romain.le-bail@eleves.ec-nantes.fr>
2014-12-21 16:08:25 +0100

    INIT COMMIT YOLO YOLO

---
+++ src/fractalviewer/FractalViewer.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fractalviewer;
+
+/**
+ *
+ * @author IAZERTYUIOPI
+ */
+public class FractalViewer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //ViewWindow window = new ViewWindow(1600,800,0,0,-0.8,0.156);
+        ViewWindow window = new ViewWindow(1600,800,-1,0);
+        window.zoomFactor=1.5;
+        window.draw();
+    }
+    
+}
